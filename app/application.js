5ebebe0735dcf9f37482e1f9651781053d2ce1f7
Bogdan Filimon <filimonbogdan89@gmail.com>
2022-04-24 14:12:11 +0300

    log connection string

--- app/application.js
+++ app/application.js
@@ -11,38 +11,38 @@ const app = express();
 const port = process.env.PORT;
 
 function startServer() {
-    app.listen(port, () => console.log(`Running on port ${port}...`));
+  app.listen(port, () => console.log(`Running on port ${port}...`));
 }
 
 const db = process.env.CONNECTION_STRING;
+console.log(process.env.CONNECTION_STRING);
 
 function startDatabase() {
-    mongoose.connect(db, (err) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log("MongoDB database connection established successfully");
-    });
+  mongoose.connect(db, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("MongoDB database connection established successfully");
+  });
 }
 
 function initRouters() {
-    app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "*");
-        res.header("Access-Control-Allow-Methods", "*");
-        next();
-    });
-    app.use(express.json());
-    app.use("/user", userRouter);
-    app.use("/movie", movieRouter);
-    app.use("/hall", hallRouter);
-    app.use("/reservation", reservationRouter);
-
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    next();
+  });
+  app.use(express.json());
+  app.use("/user", userRouter);
+  app.use("/movie", movieRouter);
+  app.use("/hall", hallRouter);
+  app.use("/reservation", reservationRouter);
 }
 
 (function run() {
-    startServer();
-    startDatabase();
-    initRouters();
+  startServer();
+  startDatabase();
+  initRouters();
 })();
